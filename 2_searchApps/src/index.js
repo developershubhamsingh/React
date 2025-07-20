44175d488bb948c22ef83d4bbdefa67dad3a473f
developershubhamsingh <shubhamsinghworkid@gmail.com>
2025-07-20 07:54:54 -0700

    .

--- 2_searchApps/src/index.js
+++ 2_searchApps/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app"
+import App from "./componentss/app";
+ 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
